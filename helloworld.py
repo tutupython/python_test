6e1cb14b6e93e36e04118077f9033e63d462af6c
tutupython <13805003351@139.com>
2018-03-06 22:40:51 +0800

    爬虫联系 201803062240

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,13 @@
 import re
-result=re.match('com', 'www.runoob.com')
+content= "hello 1234567 World_This is hi a regex Demo"
+pattern=re.compile('h.*? (.)(.)')
+result=re.findall(pattern,content)
 if result:
-    print(re.match('com', 'www.runoob.com').group())
+    print(result)
+    print(type(result))
+    for rec in result:
+        print(type(rec))
+        print(rec)
+
 else:
     print(result)
--- python_test.py
+++ python_test.py
@@ -9,24 +9,28 @@ class MovieTop(object):
         self.file_path='E:\movie_spider.txt'
         self.url = 'https://movie.douban.com/top250?start=' + str(self.start)
     def get_page(self):
-
         try:
             # while self.start<255:
             data = requests.get(self.url, headers=self.headers).content
+            # print('data1',type(data))
+            data = data.decode('utf-8')
+            # print('data2',type(data))
+            data=re.sub('\n','',data)
+
             return data
-                # page_content.append(page)
-                # print(page_content)
-            # return page_content
         except requests.URLError as URLerr:
             if hasattr(URLerr,'reason'):
                 print('抓取失败：',URLerr.reason)
+
+    def get_movie_info(self,page):
+        print(type(page))
+
+        parttern=re.compile('<span class="title">(.*?)</span>.*?<span class="title">&nbsp;/&nbsp;(.*?)</span>')
+        result=re.findall(parttern,page)
+        print(result)
     def main(self):
-
         page=self.get_page()
-        print(page)
-    def get_movie_info(self):
-        return 0
-
+        self.get_movie_info(page)
 
 # if __name__ == '__main__':
 
