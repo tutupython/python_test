31d0a4083984c0f4654269e63ec1458ac032cc5d
tutupython <13805003351@139.com>
2018-03-07 16:22:21 +0800

    20180307 Django学习

--- HelloWorld/Hello/views.py
+++ HelloWorld/Hello/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render_to_response,render,get_object_or_404
+from .loginform import LoginForm
 # Create your views here.
+
+def login(request):
+    if request.method == 'GET':
+        form = LoginForm()
+        return render_to_response('login.html')
+    else:
+        return HttpResponse("hello world")
